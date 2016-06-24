bbbb506eae57f43365eec526eeb50ac762d07942
Luke Jackson <luke.jackson@bedegaming.com>
2016-06-24 18:40:44 -0400

    Add files via upload

---
+++ background.js
@@ -0,0 +1,49 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+chrome.browserAction.onClicked.addListener(function() {
+  chrome.tabs.captureVisibleTab(function(screenshotUrl) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
+      chrome.identity.getProfileUserInfo(function (userInfo) {
+
+        var optimize = function(url, scale, quality){
+           var shot = new Image();
+           shot.src = url;
+           var cvs = document.createElement('canvas');
+           cvs.width = shot.naturalWidth*scale;
+           cvs.height = shot.naturalHeight*scale;
+           cvs.getContext('2d')
+              .drawImage(shot, 0, 0, shot.naturalWidth*scale, shot.naturalHeight*scale);
+           return cvs.toDataURL('image/jpeg', quality);
+        };
+
+        var browse = {
+          browser: 'lukejacksonn',
+          title: arrayOfTabs[0].title,
+          shot: optimize(screenshotUrl, 0.5, 1),
+          url: arrayOfTabs[0].url
+        };
+
+        window.alert(JSON.stringify(browse));
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === 4) {
+
+            window.alert(xhr.responseText);
+            var res = JSON.parse(xhr.responseText);
+            var viewTabUrl = chrome.extension.getURL('screenshot.html?browser=' + res.browser);
+            chrome.tabs.create({ url: viewTabUrl }, function() {});
+          }
+        };
+
+        xhr.open('POST', 'https://ih41lln30j.execute-api.eu-west-1.amazonaws.com/beta/browses', true);
+        xhr.send(JSON.stringify(browse));
+
+      });
+    });
+  });
+});
---
+++ screenshot.js
@@ -0,0 +1,76 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    };
+}
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+         var pair = vars[i].split("=");
+         if(pair[0] == variable){return pair[1];}
+  }
+  return(false);
+}
+
+function timeSince(date) {
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var interval = Math.floor(seconds / 31536000);
+    if (interval >= 1) return interval + ' years';
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) return interval + ' months';
+    interval = Math.floor(seconds / 86400);
+    if (interval >= 1) return interval + ' days';
+    interval = Math.floor(seconds / 3600);
+    if (interval >= 1) return interval + ' hours';
+    interval = Math.floor(seconds / 60);
+    if (interval >= 1) return interval + ' minutes';
+    return Math.floor(seconds) + ' seconds';
+}
+
+var xhr = new XMLHttpRequest();
+
+var browseItemComponent = function (browse) {
+  return `
+    <browse->
+      <a target="_blank" href="${browse.url}">
+        <img src="${browse.shot}" />
+      </a>
+      <article>
+        <title->${browse.title}</title->
+        <info->Shot by <a href="/${browse.browser}">${browse.browser}</a> ${timeSince(browse.published)} ago</info->
+        <browser-count>${browse.browsers.length} Other Browsers</browser-count>
+      </article>
+    </browse->
+  `;
+};
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4) {
+    var feed = document.querySelector('browses-feed');
+    var browses = JSON.parse(xhr.responseText);
+    var templates = browses.map(function(browse) {
+      return browseItemComponent(browse);
+    });
+
+    feed.innerHTML = templates.sort(dynamicSort('published')).join('');
+  }
+};
+
+xhr.open('GET', `https://ih41lln30j.execute-api.eu-west-1.amazonaws.com/beta/browses/${getQueryVariable('browser')}`, true);
+xhr.send();
+
+document.querySelector('user-name h1').innerHTML = getQueryVariable('browser');
