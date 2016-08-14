0ae90dcd03fbcfcae343d2ccdb1eab7d4bf625cd
Luke Jackson <lukejacksonn@gmail.com>
2016-08-14 18:12:48 -0400

    Removes XHR from screenshot script

--- screenshot.js
+++ screenshot.js
@@ -1,27 +1,9 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-'use strict';
-
-function dynamicSort(property) {
-    var sortOrder = 1;
-    if(property[0] === "-") {
-        sortOrder = -1;
-        property = property.substr(1);
-    }
-    return function (a,b) {
-        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
-        return result * sortOrder;
-    };
-}
-
 function getQueryVariable(variable) {
   var query = window.location.search.substring(1);
   var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-         var pair = vars[i].split("=");
-         if(pair[0] == variable){return pair[1];}
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] == variable){return pair[1];}
   }
   return(false);
 }
@@ -40,8 +22,6 @@ function timeSince(date) {
     if (interval >= 1) return interval + ' minutes';
     return Math.floor(seconds) + ' seconds';
 }
-
-var xhr = new XMLHttpRequest();
 
 var browseItem = function (browse) {
   return `
